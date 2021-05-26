438ea265b7079ae25514c67917338c30ccf42c45
Jiahui Chen <Jeffreychen14898@gmail.com>
2021-05-26 14:03:51 -0500

    drew the first triangle

---
+++ test/script.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+	const config = {
+		canvas: "canvas"
+	};
+	const renderer = new JSRenderer(config);
+}
+
+/*
+const config = {
+	canvas: "canvID",
+};
+const renderer = new JSRenderer(config);
+renderer.draw.rect(50, 50, 100, 100, {color: white});
+*/
