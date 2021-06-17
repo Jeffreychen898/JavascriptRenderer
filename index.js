902b732bd8d54d2fca86df6d3541cfb1ea4246de
Jeffrey Chen <Jeffreychen14898@gmail.com>
2021-06-17 17:25:35 -0500

    added index.ejs

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+const lineReader = require("line-reader");
 const express = require("express");
 const app = express();
 
 const PORT = 8080 || process.env.PORT;
 
+const paths = [];
+
 app.listen(PORT, () => {
-	console.log(PORT);
+	console.log(`PORT: ${PORT}`);
+
+	lineReader.eachLine("test/includes.txt", (line) => {
+		paths.push(line);
+
+		if(line.includes("main.js"))
+			console.log(`${paths.length} javascript files found!`);
+	});
 });
 
-app.use("/", express.static("test"));
+app.set("view engine", "ejs");
+
+app.use("/test", express.static("test"));
 app.use("/renderer", express.static("renderer"));
+
+app.get("/", (req, res) => {
+	res.render("index.ejs", {
+		library: paths
+	});
+});
