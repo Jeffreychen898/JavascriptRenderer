ad55571eb98613053c839ef68b9e15f530c7ebd3
Jeffrey Chen <Jeffreychen14898@gmail.com>
2021-06-17 19:26:31 -0500

    implemented condense feature in index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const fs = require("fs");
 const lineReader = require("line-reader");
 const express = require("express");
 const app = express();
 
 const PORT = 8080 || process.env.PORT;
+const VERSION = 1;
 
 const paths = [];
 
@@ -11,7 +13,6 @@ app.listen(PORT, () => {
 
 	lineReader.eachLine("test/includes.txt", (line) => {
 		paths.push(line);
-
 		if(line.includes("main.js"))
 			console.log(`${paths.length} javascript files found!`);
 	});
@@ -27,3 +28,43 @@ app.get("/", (req, res) => {
 		library: paths
 	});
 });
+
+app.get("/condense", async (req, res) => {
+	try {
+		const result = await condenseJSCode();
+		const filename = "RendererV" + VERSION + ".js";
+		fs.writeFile("library/condensed/" + filename, result, (err) => {
+			if(err)
+				res.send(err);
+			else
+				res.send("successfully minifed version " + VERSION);
+		});
+	} catch(err) {
+		res.send(err);
+	}
+});
+
+async function condenseJSCode() {
+	return new Promise((resolve, reject) => {
+		let tracker = 0;
+		let source_code = new Array(paths.length);
+		for(let i=0;i<paths.length;i++) {
+			const path = paths[i];
+
+			fs.readFile(path, "utf8", (err, data) => {
+				console.log(`reading /${path}`);
+
+				if(err)
+					reject(err);
+				else {
+					console.log(`successfully read /${path}`);
+					source_code[i] = data;
+				}
+				if(++tracker == paths.length) {
+					resolve(source_code.join(""));
+				}
+			});
+
+		}
+	});
+}
